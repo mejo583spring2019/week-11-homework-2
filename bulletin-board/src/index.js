2ecc7dce914cd705e40d58c39313509d31051887
Andrea Brucculeri <andrea@brucculeri.com>
2019-03-22 12:46:21 -0400

    started ch 4

--- bulletin-board/src/index.js
+++ bulletin-board/src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import Note from "./note";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("react-container"));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Note />, document.getElementById("react-container"));
+registerServiceWorker();
