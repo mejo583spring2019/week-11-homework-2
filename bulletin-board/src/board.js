83481914dd24aed8cba3bab2c399c52dce37e7f6
Andrea Brucculeri <andrea@brucculeri.com>
2019-03-22 14:15:26 -0400

    started ch 5

--- bulletin-board/src/board.js
+++ bulletin-board/src/board.js
@@ -7,26 +7,37 @@ class Board extends Component {
         this.state = {
             notes: [
                 {
-                    id: 33,
+                    id: 0,
                     note: 'Call Lisa'
                 },
                 {
-                    id: 34,
+                    id: 1,
                     note: 'Email john'
                 },
                 {
-                    id: 35,
+                    id: 2,
                     note: 'order ink'
                 }
             ]
         }
         this.eachNote = this.eachNote.bind(this)
+        this.update = this.update.bind(this)
+    }
+
+    update(newText, i) {
+        console.log('updating item at index i', i, newText)
+        this.setState(prevState => ({
+            notes: prevState.notes.map(
+                note => (note.id !== i) ? note : {...note, note: newText}
+            )
+        }))
     }
 
     eachNote(note, i){
         return (
             <Note key={i}
-                 index={i}>
+                 index={i}
+                 onChange={this.update}>
                 {note.note}
             </Note>
         )
