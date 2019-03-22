5c829d5732c826000ca3aecbf03bb3e64672aae4
Brooke Fisher <brookenfish1@gmail.com>
2019-03-22 09:04:33 -0400

    Add edit and remove functions and a save icon

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
 import { FaPencilAlt } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
+import { FaRegSave } from "react-icons/fa";
 class Note extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      editing: false,
+    };
     this.edit = this.edit.bind(this);
     this.remove = this.remove.bind(this);
   }
 
   edit() {
-    alert("editing note");
+    this.setState({
+      editing: true,
+    });
   }
 
   remove() {
     alert("removing note");
+  }
+
+  renderForm() {
+    return (
+      <div className="note">
+        <form>
+          <textarea />
+          <button><FaRegSave /></button>
+        </form>
+      </div>
+    );
   }
 
   render() {
