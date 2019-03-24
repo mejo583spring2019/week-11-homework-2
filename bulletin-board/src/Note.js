2a35b4398110e47e13c02244435b517fbc37fe16
Brooke Fisher <brookenfish1@gmail.com>
2019-03-23 20:19:28 -0400

    Create a board with sticky notes randomly placed

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -13,6 +13,19 @@ class Note extends Component {
     this.save = this.save.bind(this);
     this.renderForm = this.renderForm.bind(this);
     this.renderDisplay = this.renderDisplay.bind(this);
+    this.randomBetween = this.randomBetween.bind(this);
+  }
+
+  componentWillMount() {
+    this.style = {
+      right: this.randomBetween(0, window.innerWidth - 150, "px"),
+      top: this.randomBetween(0, window.innerHeight - 150, "px"),
+      transform: `rotate(${this.randomBetween(-25, 25, "deg")})`,
+    };
+  }
+
+  randomBetween(x, y, s) {
+    return x + Math.ceil(Math.random() * (y - x)) + s;
   }
 
   edit() {
@@ -35,7 +48,7 @@ class Note extends Component {
 
   renderForm() {
     return (
-      <div className="note">
+      <div className="note" style={this.style}>
         <form onSubmit={this.save}>
           <textarea ref={(input) => this._newText = input} />
           <button id="save"><FaRegSave /></button>
@@ -46,7 +59,7 @@ class Note extends Component {
 
   renderDisplay() {
     return (
-      <div className="note">
+      <div className="note" style={this.style}>
         <p>{this.props.children}</p>
         <span>
           <button onClick={this.edit} id="edit"><FaPencilAlt /></button>
