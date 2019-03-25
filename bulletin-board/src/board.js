3571a8e2b5ad6c440724fbcf0d502da56def1da6
Taylor Tyson1 <ttay921@live.unc.edu>
2019-03-25 00:12:49 -0400

    change

--- bulletin-board/src/board.js
+++ bulletin-board/src/board.js
@@ -7,39 +7,55 @@ class Board extends Component {
         this.state = {
             notes: [
                 {
-                    id=33
-                    note="Call Lisa"
+                    id=0
+                    Note="Call Lisa"
 
                 },
                 {
-                    id=34
-                    note="Email John"
+                    id=1
+                    Note="Email John"
+                },
+                {
+                    id=2
+                    Note="Order Printer Ink"
                 }
             ]
         }
         this.eachNote = this.eachNote.bind(this)
+        this.update = this.update.bind(this)
     }
 
-    eachNote(note, i) {
-        return (
-            <Note key={i}
-                index={i}>
-                {note.note}
-            </Note>
-        )
+    update(newText, i) {
+        console.log('updating item at index', i, newText)
+        this.setState(prevState => ({
+            notes=prevState.notes.map => (
+                note => (note.id !== i) ? note : { ...note, note: newText }
+            )
+
+        }))
+}
+
+eachNote(note, i) {
+    return (
+        <Note key={i}
+            index={i}
+            onChange={this.update}>
+            {note.note}
+        </Note>
+    )
 
 
-    }
+}
 
 
 
 
-    render() {
-        return (
-            <div className="board">
-                {this.render.state.notes.map(this.eachNote)}
-            </div>
-        )
-    }
+render() {
+    return (
+        <div className="board">
+            {this.render.state.notes.map(this.eachNote)}
+        </div>
+    )
+}
 }
 export default Board 
