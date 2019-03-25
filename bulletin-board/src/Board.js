d1b21ce99659e0dc9cc62fcfdaf3d76eceee2602
Sarah Kreitzer <sarah.anne.kreitzer@gmail.com>
2019-03-24 23:41:48 -0400

    5.1

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -8,22 +8,37 @@ class Board extends Component {
     this.state = {
       note: [
         {
-          id: 33,
+          id: 0,
           note: "Call Lisa",
         },
         {
-          id: 34,
+          id: 1,
           note: "Email John",
+        },
+        {
+          id: 2,
+          note: "Order printer ink",
         },
       ],
     };
     this.eachNote = this.eachNotebind(this);
+    this.update = this.update.bind(this);
+  }
+
+  update(newText, i) {
+    // breaking code console.log( "updating item at index", i, newText);
+    this.setState((prevState) => ({
+      notes: prevState.notes.map(
+          (note) => (note.id !== i) ? note : {...note, note: newText}
+      ),
+    }));
   }
 
   eachNote(note, i) {
     return (
       <Note key={i}
-        index={i}>
+        index={i}
+        onChange={this.update}>
         {note.note}
       </Note>
     );
