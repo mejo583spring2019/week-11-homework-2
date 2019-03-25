1471820875aec0a2f791a5f44ff7f2b48d2fb4b6
JP Rickabaugh <jprickabaugh@gmail.com>
2019-03-24 21:44:58 -0400

    ch4 #2

--- bulletin-board/src/note.js
+++ bulletin-board/src/note.js
@@ -2,13 +2,24 @@ import React, { Component } from "react";
 import { FaPencilAlt } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
 class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.edit = this.edit.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+  edit() {
+    alert("editing note");
+  }
+  remove() {
+    alert("removing note");
+  }
   render() {
     return (
       <div className="note">
         <p>Learn React</p>
         <span>
-          <button><FaPencilAlt /></button>
-          <button><FaTrashAlt /></button>
+          <button onClick={this.edit} id="edit"><FaPencilAlt /></button>
+          <button onClick={this.remove} id="remove"><FaTrashAlt /></button>
         </span>
       </div>
     );
