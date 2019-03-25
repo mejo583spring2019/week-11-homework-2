a20c71001968046cdb725bc87cd551482623ca86
Taylor Tyson1 <ttay921@live.unc.edu>
2019-03-24 23:55:38 -0400

    What I've Done

--- bulletin-board/src/board.js
+++ bulletin-board/src/board.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react',
 import Note from './note'
 
 class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: [
+                {
+                    id=33
+                    note="Call Lisa"
+
+                },
+                {
+                    id=34
+                    note="Email John"
+                }
+            ]
+        }
+    }
+
+    eachNote(note, i) {
+        return (
+            <Note key={i}
+                index={i}>
+                {note.note}
+            </Note>
+        )
+
+
+    }
+
+
+
+
     render() {
         return (
             <div className="board">
