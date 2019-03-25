79e8ce0e146aef183d05c652f23f9659c4d71912
JP Rickabaugh <jprickabaugh@gmail.com>
2019-03-25 12:05:16 -0400

    post-deadline: Fixing linting erros that were present due to copying and pasting from exercise files. Also commented out console statements to pass circleCI test. ADDENDUM: some console log statements had closing parentheses on other lines - commented those out as well to pass test. Also had to comment out the messages themselves.

--- bulletin-board/src/serviceWorker.js
+++ bulletin-board/src/serviceWorker.js
@@ -42,8 +42,8 @@ export function register(config) {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           // console.log(
-          "This web app is being served cache-first by a service " +
-              "worker. To learn more, visit https://bit.ly/CRA-PWA";
+          // "This web app is being served cache-first by a service " +
+          //   "worker. To learn more, visit https://bit.ly/CRA-PWA";
           // );
         });
       } else {
@@ -70,8 +70,8 @@ function registerValidSW(swUrl, config) {
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
                 // console.log(
-                "New content is available and will be used when all " +
-                  "tabs for this page are closed. See https://bit.ly/CRA-PWA.";
+              //  "New content is available and will be used when all " +
+              //    "tabs for this page are closed. See https://bit.ly/CRA-PWA.";
                 // );
 
                 // Execute callback
@@ -121,7 +121,7 @@ function checkValidServiceWorker(swUrl, config) {
       })
       .catch(() => {
         // console.log(
-        "No internet connection found. App is running in offline mode.";
+      //  "No internet connection found. App is running in offline mode.";
         // );
       });
 }
