f3c7496daf5fcb4343a679ee5880e9979b6575f0
JP Rickabaugh <jprickabaugh@gmail.com>
2019-03-24 23:08:35 -0400

    ch6 complete

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -61,12 +61,12 @@ class Board extends Component {
 
   eachNote(note, i) {
     return (
-      <Note key={i}
-				  index={i}
-				  onChange={this.update}
-				  onRemove={this.remove}>
-				  {note.note}
-		    </Note>
+      <Note key={note.id}
+		  index={note.id}
+		  onChange={this.update}
+		  onRemove={this.remove}>
+		  {note.note}
+	  </Note>
     );
   }
 
